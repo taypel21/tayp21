049171dfe11c91614e7ac8a5a818f508d9854bfb
taypel21 <taypelzer68@gmail.com>
2021-04-02 11:21:32 -0400

    add index file

---
+++ index.js
@@ -0,0 +1,9 @@
+function valueCheck(firstNumber, secondNumber){ 
+    if(firstNumber===secondNumber){
+        return true   
+    }else{
+        return false
+    }
+}
+
+console.log(valueCheck(5,5))
